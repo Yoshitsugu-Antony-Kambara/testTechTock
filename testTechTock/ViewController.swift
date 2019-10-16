4f512cc22517d057b7c129b42874155557844d42
Yoshitsugu Kambara <tsugu1999213@gmail.com>
2019-10-16 19:40:53 +0900

    qiitaApi取得

--- testTechTock/ViewController.swift
+++ testTechTock/ViewController.swift
@@ -9,33 +9,44 @@
 import UIKit
 import Alamofire
 import SwiftyJSON
+import SafariServices
+
 
 class ViewController: UIViewController {
     
-    var tableView = UITableView()
-    var articles: [String]!
-
+    @IBOutlet var tableView: UITableView!
+    var articles: [[String]] = []
+    var selectUrl:String!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = "記事一覧"
         
         tableView.dataSource = self
-        view.addSubview(tableView)
+        
         
         getArticles()
+    
         
     }
 
     func getArticles() {
-        Alamofire.request("URLofEndPoint")
+        Alamofire.request("https://qiita.com/api/v2/items")
             .responseJSON { response in
-                print(response.result.value as Any)
+                //print(response.result.value as Any)
+                guard let object = response.result.value else {
+                    return
+                }
+                
                 
                 let json = JSON(object)
+                
                 json.forEach { (_,json) in
-                    let article: String? = json["key"].string
-                    self.articles.append(articles)
+                    let article: [String] = [json["title"].string!, json["url"].string!]
+                    self.articles.append(article)
                 }
                 self.tableView.reloadData()
+                
         }
 
     }
@@ -51,10 +62,26 @@ extension ViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
-        cell.textLabel?.text = articles[indexPath.row]
+        cell.textLabel?.text = articles[indexPath.row][0]
         return cell
     }
     
     
+    
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(indexPath.row)
+        //print("\(indexPath.row)th cell is celected")
+        
+        //tableView.deselectRow(at: indexPath, animated: true)
+        selectUrl = articles[indexPath.row][1]
+        guard let url = URL(string: selectUrl) else { return }
+            let safariController = SFSafariViewController(url: url)
+            present(safariController, animated: true, completion: nil)
+
+    }
+        
 }
+    
+
 
