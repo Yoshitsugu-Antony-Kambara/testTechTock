7d1b5503c1f02716911d70225cb220abbbec26b6
Yoshitsugu Kambara <tsugu1999213@gmail.com>
2019-10-16 17:36:51 +0900

    Initial Commit

---
+++ testTechTock/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testTechTock
+//
+//  Created by 神原良継 on 2019/10/16.
+//  Copyright © 2019 jp.Antony. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
