1406866b30a306221bd9a2cc3d2c494d22720338
Yoshitsugu Kambara <tsugu1999213@gmail.com>
2019-10-16 18:07:34 +0900

    first commit

--- testTechTock/ViewController.swift
+++ testTechTock/ViewController.swift
@@ -7,14 +7,54 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class ViewController: UIViewController {
+    
+    var tableView = UITableView()
+    var articles: [String]!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tableView.dataSource = self
+        view.addSubview(tableView)
+        
+        getArticles()
+        
     }
 
+    func getArticles() {
+        Alamofire.request("URLofEndPoint")
+            .responseJSON { response in
+                print(response.result.value as Any)
+                
+                let json = JSON(object)
+                json.forEach { (_,json) in
+                    let article: String? = json["key"].string
+                    self.articles.append(articles)
+                }
+                self.tableView.reloadData()
+        }
 
+    }
+    
+    
+
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return articles.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = articles[indexPath.row]
+        return cell
+    }
+    
+    
 }
 
